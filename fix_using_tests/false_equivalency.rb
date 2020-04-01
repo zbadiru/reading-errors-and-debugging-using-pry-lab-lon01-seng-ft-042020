2166350279efcf8933384a25d661deef32bdba9a
Zakari Badiru <zbadiru@live.co.uk>
2020-04-01 16:24:50 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def get_user_input
   gets.chomp
 end
@@ -11,9 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  if num == 1
+    "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,10 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    #"ssssssssss" + string
+    ('s' * 10) + string
   else
     string
   end
 end
+ 
